8330380b52802786f7e44989863de593260c31a1
artur musheghyan <artarmmush@gmail.com>
2022-02-01 11:22:27 +0400

    Implement gltf loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,54 @@
-const path = require('path')
+const path = require("path");
+
+console.warn(__dirname);
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main.ts',
+  mode: "development",
+  entry: "./src/main.ts",
   devServer: {
     port: 9000,
     static: {
       serveIndex: true,
-      directory: __dirname
-    }
+      directory: __dirname,
+    },
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
-    ]
+      {
+        test: /\.(gltf)$/,
+        use: [
+          {
+            loader: "gltf-webpack-loader",
+          },
+        ],
+      },
+
+      {
+        test: /\.(bin)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {},
+          },
+        ],
+      },
+    ],
   },
+
   watchOptions: {
-    ignored: /node_modules/
-  }
-}
+    ignored: /node_modules/,
+  },
+};
