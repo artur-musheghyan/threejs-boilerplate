cbc7f58b62f90b50e3cdca162d662ae36d1e9413
artur musheghyan <artarmmush@gmail.com>
2022-02-08 13:29:08 +0400

    Initial commit.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,16 @@ import {
   AmbientLight,
   AnimationMixer,
   Color,
+  Mesh,
+  MeshStandardMaterial,
   PerspectiveCamera,
   PointLight,
   Scene,
   Vector3,
   WebGLRenderer,
 } from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { CustomStats } from "../utils/custom-stats";
 import { Brick } from "./brick";
 
@@ -20,16 +23,39 @@ export class App {
     0.1,
     10000
   );
+  public readonly canvasElement = document.getElementById(
+    "main-canvas"
+  ) as HTMLCanvasElement;
+
   public readonly renderer = new WebGLRenderer({
     antialias: true,
-    canvas: document.getElementById("main-canvas") as HTMLCanvasElement,
+    canvas: this.canvasElement,
   });
 
-  private brick: Brick;
+  private brick!: Brick;
+  private fox!: GLTF;
   private _mixer!: AnimationMixer;
   private _stats: CustomStats;
 
   constructor() {
+    var controls = new OrbitControls(this.camera, this.canvasElement);
+
+    controls.rotateSpeed = 0.3;
+    controls.zoomSpeed = 0.9;
+
+    controls.minDistance = 0.2;
+    controls.maxDistance = 200;
+
+    controls.minPolarAngle = 0; // radians
+    controls.maxPolarAngle = Math.PI / 2; // radians
+
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+
+    // renderCalls.push(function () {
+    //   controls.update();
+    // });
+
     const gltfLoader = new GLTFLoader();
     this.scene.add(new AmbientLight(0x404040));
 
@@ -39,24 +65,70 @@ export class App {
 
     this.scene.add(light);
 
-    this.brick = new Brick();
-    this.scene.add(this.brick);
+    // this.brick = new Brick();
+    // this.scene.add(this.brick);
 
-    this.camera.position.set(10, 10, 10);
+    // const a = new Brick();
+    // this.scene.add(a);
+    // a.position.set(4, 0, 0);
+    // a.scale.set(2, 0.4, 1);
+
+    // const b = new Brick();
+    // this.scene.add(b);
+    // b.position.set(-4, 0, 0);
+
+    this.camera.position.set(-40, 40, 40);
     this.camera.lookAt(new Vector3(0, 0, 0));
 
     this.renderer.setSize(innerWidth, innerHeight);
-    this.renderer.setClearColor(new Color("rgb(0,0,0)"));
+    this.renderer.setClearColor(new Color("rgb(160,160,160)"));
 
-    gltfLoader.load("/assets/Fox/glTF/Fox.gltf", (gltf) => {
-      gltf.scene.scale.set(0.025, 0.025, 0.025);
-      gltf.scene.position.set(-2, 0, 1);
-      this.scene.add(gltf.scene);
+    // setTimeout(() => {
+    //   console.warn(this.renderer.info.memory);
+    //   // this.scene.remove(this.brick);
+    // }, 1000);
 
-      // Animation
-      this._mixer = new AnimationMixer(gltf.scene);
-      const action = this._mixer.clipAction(gltf.animations[2]);
-      action.play();
+    gltfLoader.load("/assets/FlightHelmet/glTF/FlightHelmet.gltf", (data) => {
+      let object = data.scene;
+      object.scale.set(10, 10, 10);
+      object.position.set(0, 0, 0);
+      this.scene.add(object);
+      setTimeout(() => {
+        object.children.forEach((mesh) => {
+          (<Mesh>mesh).geometry.dispose();
+          data.scene.traverse((child) => {
+            // @ts-ignore
+            if (child.material) {
+              const {
+                alphaMap,
+                aoMap,
+                bumpMap,
+                displacementMap,
+                emissiveMap,
+                envMap,
+                lightMap,
+                map,
+                metalnessMap,
+                normalMap,
+                roughnessMap,
+              } = <MeshStandardMaterial>(<Mesh>child).material;
+              alphaMap?.dispose();
+              aoMap?.dispose();
+              bumpMap?.dispose();
+              displacementMap?.dispose();
+              emissiveMap?.dispose();
+              envMap?.dispose();
+              lightMap?.dispose();
+              map?.dispose();
+              metalnessMap?.dispose();
+              normalMap?.dispose();
+              roughnessMap?.dispose();
+            }
+          });
+        });
+
+        this.scene.remove(object);
+      }, 2000);
     });
 
     this._stats = new CustomStats(this.renderer);
--- src/app/brick.ts
+++ src/app/brick.ts
@@ -1,9 +1,11 @@
 import { BoxGeometry, Mesh, MeshPhongMaterial } from "three";
 
 export class Brick extends Mesh {
+  private static customGeometry: BoxGeometry = new BoxGeometry(3, 3, 3);
+
   constructor() {
     super();
-    this.geometry = new BoxGeometry(3, 3, 3);
+    this.geometry = Brick.customGeometry;
     this.material = new MeshPhongMaterial({
       color: 0x4040c0,
       shininess: 0.4,
